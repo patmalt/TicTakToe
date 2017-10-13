5653a22b6940ec1b6663151f56dee5014dba3f89
Patrick Maltagliati <patmalt@gmail.com>
2017-10-12 23:41:27 -0500

    Initial Commit

---
+++ Blah/Blah.h
@@ -0,0 +1,19 @@
+//
+//  Blah.h
+//  Blah
+//
+//  Created by Patrick Maltagliati on 10/12/17.
+//  Copyright © 2017 Patrick Maltagliati. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Blah.
+FOUNDATION_EXPORT double BlahVersionNumber;
+
+//! Project version string for Blah.
+FOUNDATION_EXPORT const unsigned char BlahVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Blah/PublicHeader.h>
+
+
